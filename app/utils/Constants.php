66283a3258a681f60d73aa64e801f0bbd9d30dff
Andresperezcapri1 <andresperezcapri@gmail.com>
2024-04-29 14:41:01 +0200

    Desarrollo front de las distintas vistas

--- app/utils/Constants.php
+++ app/utils/Constants.php
@@ -1,5 +1,5 @@
 <?php
 define('ROUTE_APP', dirname(dirname(__FILE__)));
 define('ROUTE_PUBLIC',dirname(dirname(dirname(__FILE__))));
-define('ROUTE_URL','http://localhost/tfg');
+define('ROUTE_URL','http://localhost/proyectodaw');
 define('NAME_WEB','SITIO_WEB');
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-black">
         <div class="container">
-            <a class="navbar-brand" href="/tfg/">FILMFAN</a>
+            <a class="navbar-brand" href="/proyectodaw/">FILMFAN</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -9,7 +9,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="/tfg/movie/">Movies</a>
+                        <a class="nav-link" aria-current="page" href="/proyectodaw/movie/">Movies</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Perfil</a>
